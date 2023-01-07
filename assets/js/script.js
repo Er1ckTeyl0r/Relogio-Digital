ba9cca69e0571b0b3b4b5671f24e33bdcd534673
ErickTeylor <erickteylor2601@gmail.com>
2023-01-07 16:21:44 -0300

    Relogio Digital
    Adicionei mensagens e cores diferentes de acordo com a hora

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,15 @@ const hora = document.getElementById('hora');
 const minuto = document.getElementById('minuto');
 const segundo = document.getElementById('segundo');
 
+const frase = document.getElementById('frase');
+
 const relogio = setInterval(function time () {
     let dataToday = new Date()
     let hr = dataToday.getHours()
     let min = dataToday.getMinutes()
     let seg = dataToday.getSeconds()
+
+    hr = 1
 
     if (hr < 10) hr = '0' + hr;
 
@@ -17,5 +21,24 @@ const relogio = setInterval(function time () {
     hora.textContent = hr;
     minuto.textContent = min;
     segundo.textContent = seg;
+
+    if (hr >=5 && hr < 12) {
+        document.body.style.background = 'linear-gradient(120deg, rgb(212, 191, 119), rgb(116, 116, 38))';
+        frase.textContent = 'Bom Dia!';
+    }
+
+    if (hr >=12 && hr < 18) {
+        document.body.style.background = 'linear-gradient(120deg, rgb(218, 137, 32), rgb(175, 60, 45))';
+        frase.textContent = 'Boa Tarde!';
+    }
+
+    else if (hr >= 18 && hr <=23) { 
+        document.body.style.background = 'linear-gradient(120deg, rgb(32, 26, 26), rgb(42, 39, 90))';
+        frase.textContent = 'Boa Noite!';
+    }
+    else {
+        document.body.style.background = 'linear-gradient(120deg, rgb(32, 26, 26), rgb(42, 39, 90))';
+        frase.textContent= 'Boa Madrugada!'
+    }
 
 })
